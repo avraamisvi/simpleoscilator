c63990e26c5b693d2f8f2ae6165f894b7127d53e
abraao.isvi <abraao.isvi@gmail.com>
2015-06-02 19:03:27 -0300

    creating

--- src/org/kcsound/simpleoscilator/SimpleOscilator.kt
+++ src/org/kcsound/simpleoscilator/SimpleOscilator.kt
@@ -1,3 +1,32 @@
+import com.google.gson.JsonObject;
+
 class SimpleOscilator : Instrument {
   override val type: String = "SimpleOscilator";
+
+  /**
+   * Generate body content.
+   */
+  override public fun body():String {
+    return """
+             krnd  randomh 40, 440, 1	; produce random values
+             ain   poscil3 .6, krnd, giSine
+             kline line    1, p3, 0    	; straight line
+             aL,aR pan2    ain, kline	; sent across image
+                   outs    aL, aR
+         """
+  }
+
+  /**
+   * Generate globals to be added to the score.
+   */
+  override public fun  globals():String {
+    return "giSine ftgen 0, 0, 2^10, 10, 1";
+  }
+
+  /**
+   * Compiles the instrument.
+   */
+   override public fun compile(data:JsonObject) {
+
+   }
 }
